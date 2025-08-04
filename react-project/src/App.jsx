2fc5f0e51abb85093838c3bc46842af7ebf6fb5c
KrrishSak <krrishsakhuja@gmail.com>
2025-08-04 19:47:47 +0530

    Learning  Basics of React project

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title}) => {
   const [hasliked, sethasLinked] = useState(false);
+  const [count, setCount] = useState(0);
   
+  useEffect(() => {
+    console.log(`${title} movie has been liked: ${hasliked}`)
+  }, [hasliked])
   return ( 
     
-    <div className='Card'>
-    <h2> {title} </h2>
+    <div className='Card' onClick={() => setCount(count + 1)}>
+    <h2> {title} <br/> {count || null} </h2>
     <button onClick={() => 
-      sethasLinked(true)
+      sethasLinked(!hasliked)
     }>
       {hasliked ? '❤️' : '💟'}
     </button>
@@ -20,9 +24,8 @@ const Card = ({title}) => {
 }
 
 const App = () => {
-  
   return ( 
-    <div className='class-container'>
+    <div className='class-container' >
       <Card title = "star wars"/>
       <Card title = "Avatar"/>
       <Card title = "Lion King"/>
